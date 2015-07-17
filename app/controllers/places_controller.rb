a60c433eb9357fa5b94659c8efa3454bcc629745
bettyc <bettyc@uchicago.edu>
2015-07-17 12:26:49 -0500

    hw4-commit1

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,24 +20,49 @@ class PlacesController < ApplicationController
     redirect_to root_path
   end
 
+  def create
+    @place = Place.new
+    @place.title = params["title"]
+    @place.price = params["price"]
+    @place.photo_url = params["url"]
+    @place.desc = params["desc"]
+    @place.save
+    redirect_to root_url
+  end
+
   def edit
     @place = Place.find(params[:id])
   end
 
-  def update
-    @place = Place.find(params[:id])
-    @place.update title: params[:title],
-                 price: (params[:price].to_f * 100).to_i,
-                 photo_url: params[:url],
-                 desc: params[:desc]
+  #def update
+  #  @place = Place.find(params[:id])
+  #  @place.update title: params[:title],
+  #               price: (params[:price].to_f * 100).to_i,
+  #               photo_url: params[:url],
+  #               desc: params[:desc]
 
+  #  redirect_to "/places/#{@place.id}"
+  #end
+
+  def update
+    @place = Place.find_by(:id => params["id"])
+    @place.title = params[:title]
+    @place.price = params[:price]
+    @place.photo_url = params[:url]
+    @place.desc = params[:desc]
+    @place.save
     redirect_to "/places/#{@place.id}"
   end
 
-  def delete
-    Place.delete(params[:id])
-    redirect_to root_path
-  end
+  #def delete
+  #  Place.delete(params[:id])
+  #  redirect_to root_path
+  #end
 
+  def destroy
+    @place = Place.find_by(:id => params["id"])
+    @place.delete
+    redirect_to "/places"
+  end
 
 end
